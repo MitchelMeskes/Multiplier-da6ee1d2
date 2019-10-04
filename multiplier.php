a225fd0d6318c6508c3f0acf79fb2860a7f8b6ff
Mitchel Meskes <2115621@talnet.nl>
2019-10-04 12:09:56 +0200

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Hallo Gebruiker, Wat wilt u keer elkaar doen?\n";
+$cijfer1 = readline();
+echo "En Wat Is uw Tweede Nummer?\n";
+$cijfer2 = readline();
+echo "Dit Is Uw Antwoord: " .$cijfer1 * $cijfer2;
+
+
+
+?>
